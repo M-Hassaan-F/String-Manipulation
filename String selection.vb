eb9b69946197906e1765f86715b4d33919a315f3
M-Hassaan-F <55790227+M-Hassaan-F@users.noreply.github.com>
2019-10-06 16:46:56 +0500

    string selection
    selects first word of first string and second word from second string and merges them

---
+++ String selection.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim firstSTR As String
+        Dim SecondSTR As String
+        Dim spacepos As Integer
+        Dim Fname As String
+        Dim Lname As String
+
+        firstSTR = ""
+        SecondSTR = ""
+        spacepos = 0
+        Fname = ""
+        Lname = ""
+
+
+
+        Console.Write("Enter first string: ")
+        firstSTR = Console.ReadLine
+
+        Console.Write("enter second string: ")
+        SecondSTR = Console.ReadLine
+
+        spacepos = InStr(firstSTR, " ")
+        Fname = Left(firstSTR, spacepos)
+
+        spacepos = InStr(SecondSTR, " ")
+        Lname = Mid(SecondSTR, spacepos + 1, Len(SecondSTR))
+
+        Console.Write(Fname & Lname)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
