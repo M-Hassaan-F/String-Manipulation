aa681a32a28dd1f8920b36780239d950d5c362aa
M-Hassaan-F <55790227+M-Hassaan-F@users.noreply.github.com>
2019-10-18 16:30:31 +0500

    alphabet and digit counter
    Program that enters a string and counts number of alphabets and digits
    ignoring any special characters

---
+++ alphabet and digit counter.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim count As Integer
+        Dim digit As Integer
+        Dim alpha As Integer
+        Dim STR As String
+        Dim NextChar As Char
+
+        count = 0
+        alpha = 0
+        digit = 0
+        STR = ""
+        NextChar = ""
+
+        Console.Write("enter string: ")
+        STR = Console.ReadLine
+
+        For count = 1 To Len(STR)
+            NextChar = Mid(STR, count, 1)
+            NextChar = UCase(NextChar)
+            If Asc(NextChar) >= 65 And Asc(NextChar) <= 90 Then
+                alpha = alpha + 1
+            Else
+                If Asc(NextChar) >= 47 And Asc(NextChar) <= 57 Then
+                    digit = digit + 1
+                End If
+            End If
+        Next
+
+        Console.WriteLine("number of digits: " & digit)
+        Console.Write("number of alphabets: " & alpha)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
