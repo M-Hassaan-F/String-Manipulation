03945cff06b1d474cc5e3982e3ba253218d91c51
M-Hassaan-F <55790227+M-Hassaan-F@users.noreply.github.com>
2019-10-18 17:12:27 +0500

    vowel counter
    program that counts the number of vowels in a string

---
+++ vowel counter.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim vowels As String
+        Dim STR As String
+        Dim NextChar As Char
+        Dim NextCharSTR As Char
+        Dim vcount As Integer
+        Dim count As Integer
+        Dim count2 As Integer
+
+        vowels = "aeiou"
+        STR = ""
+        NextChar = ""
+        NextCharSTR = ""
+        vcount = 0
+        count = 0
+        count2 = 0
+
+        Console.Write("enter string: ")
+        STR = Console.Readline
+
+        For count = 1 To Len(vowels)
+            NextChar = Mid(vowels, count, 1)
+            For count2 = 1 To Len(STR)
+                NextCharSTR = Mid(STR, count2, 1)
+                If NextChar = NextCharSTR Then
+                    vcount = vcount + 1
+                End If
+            Next
+        Next
+
+        Console.Write("Number of vowels: " & vcount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
