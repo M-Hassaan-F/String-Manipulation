55d33a33a0f051d47eee268cd6b67c7c2a933edf
M-Hassaan-F <55790227+M-Hassaan-F@users.noreply.github.com>
2019-10-20 22:07:57 +0500

    Pangram checker
    program that check if entered string is a pangram( sentence containing all alphabets)

---
+++ pangram checker.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim alphabets As String
+        Dim STR1 As String
+        Dim NextChar As Char
+        Dim NextChar2 As Char
+        Dim IsPangram As Boolean
+        Dim C1 As Integer
+        Dim C2 As Integer
+
+        alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        STR1 = ""
+        NextChar = ""
+        NextChar2 = ""
+        IsPangram = True
+        C1 = 0
+        C2 = 0
+
+        Console.Write("Enter string: ")
+        STR1 = Console.ReadLine
+        STR1 = UCase(STR1)
+
+        For C1 = 1 To Len(alphabets)
+            NextChar = Mid(alphabets, C1, 1)
+            If InStr(STR1, NextChar) = 0 Then
+                IsPangram = False
+                Exit For
+            End If
+        Next
+
+        If IsPangram = True Then
+            Console.Write("string is pangram")
+        Else
+            Console.Write("string is not pangram")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
